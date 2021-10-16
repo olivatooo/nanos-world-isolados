0e7a1592e581348e25c87a33362ddd78ed1a141e
olivato <olivatooo@gmail.com>
2021-10-16 20:02:40 -0300

    :sparkles: Adds {Sound} to be played in all clients

--- Client/Index.lua
+++ Client/Index.lua
@@ -1,2 +1,12 @@
 Package.Require("Isolado.lua")
 Package.Require("HitMarker.lua")
+
+
+Events.Subscribe("SpawnSound", function(location, sound_asset, is_2D, volume, pitch)
+	Sound(location, sound_asset, is_2D, true, SoundType.SFX, volume or 1, pitch or 1)
+end)
+
+Events.Subscribe("SpawnSoundAttached", function(object, sound_asset, is_2D, auto_destroy, volume, pitch)
+	local sound = Sound(object:GetLocation(), sound_asset, is_2D, auto_destroy ~= false, SoundType.SFX, volume or 1, pitch or 1)
+	sound:AttachTo(object, AttachmentRule.SnapToTarget, "", 0)
+end)
