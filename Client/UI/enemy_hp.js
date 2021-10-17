2a8680e4bd68e391b0c0443a5e66b7143b70a466
Otavio Araujo <otavio.netoca@gmail.com>
2021-10-17 15:07:51 -0300

    Fixes shield explosion animation when it is destroyed

--- Client/UI/enemy_hp.js
+++ Client/UI/enemy_hp.js
@@ -1,57 +1,47 @@
 function EnemyHP(actual, total) {
-  let field = document.getElementById("enemy-hp");
-  if (actual == 0 || total == 0) {
-    field.classList.add("explode");
-    let enemy_hp_bar = document.getElementById("enemy-hp-bar");
-    let enemy_hp_number = document.getElementById("enemy-hp-number");
-    enemy_hp_number.innerHTML = "DEAD";
-    field.setAttribute("style", "display: block;");
-    enemy_hp_bar.setAttribute("style", "width: 0%;");
+    let field = document.getElementById('enemy-hp');
+    if (actual == 0 || total == 0) {
+        field.classList.add('explode');
+        let enemy_hp_bar = document.getElementById('enemy-hp-bar');
+        let enemy_hp_number = document.getElementById('enemy-hp-number');
+        enemy_hp_number.innerHTML = 'DEAD';
+        field.setAttribute('style', 'display: block;');
+        enemy_hp_bar.setAttribute('style', 'width: 0%;');
 
-    setTimeout(
-      function (e) {
-        e.setAttribute("style", "display: none;");
-      },
-      950,
-      field
-    );
-  } else {
-    field.classList.remove("explode");
-    let enemy_hp_bar = document.getElementById("enemy-hp-bar");
-    let enemy_hp_number = document.getElementById("enemy-hp-number");
-    enemy_hp_number.innerHTML = actual + "/" + total;
-    let progress = (100 * actual) / total;
-    field.setAttribute("style", "display: block;");
-    enemy_hp_bar.setAttribute("style", "width:" + progress + "%;");
-  }
+        setTimeout(
+            function (e) {
+                e.setAttribute('style', 'display: none;');
+            },
+            950,
+            field
+        );
+    } else {
+        field.classList.remove('explode');
+        let enemy_hp_bar = document.getElementById('enemy-hp-bar');
+        let enemy_hp_number = document.getElementById('enemy-hp-number');
+        enemy_hp_number.innerHTML = actual + '/' + total;
+        let progress = (100 * actual) / total;
+        field.setAttribute('style', 'display: block;');
+        enemy_hp_bar.setAttribute('style', 'width:' + progress + '%;');
+    }
 }
-Events.Subscribe("EnemyHP", EnemyHP);
+Events.Subscribe('EnemyHP', EnemyHP);
 
 function EnemyShield(actual, total) {
-  let field = document.getElementById("enemy-shield");
-  if (actual == 0 || total == 0) {
-    field.classList.add("explode");
-    let enemy_hp_bar = document.getElementById("enemy-shield-bar");
-    let enemy_hp_number = document.getElementById("enemy-shield-number");
-    enemy_hp_number.innerHTML = "DEAD";
-    field.setAttribute("style", "display: block;");
-    enemy_hp_bar.setAttribute("style", "width: 0%;");
+    let field = document.getElementById('enemy-shield');
+    let enemy_hp_bar = document.getElementById('enemy-shield-bar');
+    let enemy_hp_number = document.getElementById('enemy-shield-number');
+    field.setAttribute('style', 'display: block;');
 
-    setTimeout(
-      function (e) {
-        e.setAttribute("style", "display: none;");
-      },
-      950,
-      field
-    );
-  } else {
-    field.classList.remove("explode");
-    let enemy_hp_bar = document.getElementById("enemy-shield-bar");
-    let enemy_hp_number = document.getElementById("enemy-shield-number");
-    enemy_hp_number.innerHTML = actual + "/" + total;
-    let progress = (100 * actual) / total;
-    field.setAttribute("style", "display: block;");
-    enemy_hp_bar.setAttribute("style", "width:" + progress + "%;");
-  }
+    if (actual == 0 || total == 0) {
+        field.classList.add('explode');
+        enemy_hp_number.innerHTML = 'DEAD';
+        enemy_hp_bar.setAttribute('style', 'width: 0%;');
+    } else {
+        field.classList.remove('explode');
+        enemy_hp_number.innerHTML = actual + '/' + total;
+        let progress = (100 * actual) / total;
+        enemy_hp_bar.setAttribute('style', 'width:' + progress + '%;');
+    }
 }
-Events.Subscribe("EnemyShield", EnemyShield);
+Events.Subscribe('EnemyShield', EnemyShield);
